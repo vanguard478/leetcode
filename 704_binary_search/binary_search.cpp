dde526105784049f6fc0addc4f16706d45c60b53
Raza <mohammadraza.ahmad@still.de>
2022-12-01 16:09:42 +0100

    remove declaration in every loop

--- 704_binary_search/binary_search.cpp
+++ 704_binary_search/binary_search.cpp
@@ -6,16 +6,16 @@ class Solution {
    public:
     int search(std::vector<int>& nums, int target) {
         int low = 0, high = nums.size() - 1;
+        int mid = low + (high - low) / 2;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-
             if (nums[mid] == target)
                 return mid;
             else if (nums[mid] > target) {
                 high = mid - 1;
             } else
                 low = mid + 1;
+            mid = low + (high - low) / 2;
         }
         return -1;
     }
