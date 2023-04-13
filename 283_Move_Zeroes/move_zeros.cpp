458155bd9bd6db5bc5b7e6f53cdccfb8a97dfb86
Raza <razamailbox@gmail.com>
2023-04-13 13:38:24 +0200

    simple method

--- 283_Move_Zeroes/move_zeros.cpp
+++ 283_Move_Zeroes/move_zeros.cpp
@@ -1,22 +1,38 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-public:
+   public:
     void moveZeroes(vector<int>& nums) {
-        auto itr_zero= nums.begin();
+        auto itr_zero = nums.begin();
         auto itr = itr_zero;
-
-        while(itr_zero!= )
+        int temp;
+        while (itr_zero != nums.end() && itr != nums.end()) {
+            if (*itr_zero != 0) {
+                itr_zero++;
+                itr++;
+                continue;
+            }
+            if (*itr == 0) {
+                itr++;
+                continue;
+            }
+            if (*itr_zero == 0 && *itr != 0) {
+                *itr_zero = *itr;
+                *itr = 0;
+                itr_zero++;
+                itr++;
+            }
+        }
     }
 };
 
 // main function for testing the code
 int main() {
     Solution sol;
-    vector<int> nums = {0,1,0,3,12};
+    vector<int> nums = {1};
     sol.moveZeroes(nums);
     for (int i = 0; i < nums.size(); i++) {
         cout << nums[i] << " ";
