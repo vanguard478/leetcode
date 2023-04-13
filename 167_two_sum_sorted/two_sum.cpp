e77515464acd73e6e307ca2825121bf75b256a0f
Raza <razamailbox@gmail.com>
2023-04-13 15:45:21 +0200

    algo2 using unintialized index map

--- 167_two_sum_sorted/two_sum.cpp
+++ 167_two_sum_sorted/two_sum.cpp
@@ -10,18 +10,13 @@ class Solution {
         int size = numbers.size();
         int lookup;
 
-        for (int i = 0; i < size ; ++i) {
+        for (int i = 0; i < size; ++i) {
             lookup = target - numbers[i];
-            index_map[lookup] = i;
-        }
-        for (int i = 0; i < size ; ++i) {
-            
-            auto itr = index_map.find(numbers[i]);
+            auto itr = index_map.find(lookup);
             if (itr != index_map.end()) {
-                result[0] = i;
-                result[1] = itr->second;
-                return result;
+                return {index_map[lookup] + 1, i + 1};
             }
+            index_map[numbers[i]] = i;
         }
         return result;
     }
@@ -30,7 +25,7 @@ class Solution {
 // test code for the above function
 int main() {
     Solution sol;
-    vector<int> nums = {2, 3,4};
+    vector<int> nums = {2, 3, 4};
     int target = 6;
     vector<int> result = sol.twoSum(nums, target);
     for (int i = 0; i < result.size(); i++) {
