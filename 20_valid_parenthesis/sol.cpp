12b248ab10ba039fc4d8e245553f7904f7745139
Raza <razamailbox@gmail.com>
2023-10-04 01:03:00 +0200

    feature: 20_valid_parenthesis

---
+++ 20_valid_parenthesis/sol.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (auto ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else {
+                if (stk.empty()){
+                    return false;
+                }
+                char top = stk.top();
+                stk.pop();
+                if (
+                    ch == '(' && top != ')' || ch == '{' && top != '}' || ch == '[' && top != ']') {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
+
+// 20_valid_parenthesis
+// driver code
+int main() {
+    Solution sol;
+    string s = "()]{}";
+    bool ans = sol.isValid(s);
+    std::cout << std::boolalpha << ans << std::endl;
+    return 0;
+}
