1c437ba75af725a184e10afb81fe72d3cb378c22
Raza <razamailbox@gmail.com>
2023-11-27 13:15:00 +0530

    added code for back tracking illustration

--- 22_generate_parentheses/sol.cpp
+++ 22_generate_parentheses/sol.cpp
@@ -6,29 +6,35 @@ using namespace std;
 
 class Solution {
 public:
-  vector<string> generateParenthesis(int n) {
-    std::vector<string> result = {};
-    generateParenthesisHelper(result, "", 0, 0, n);
-    return result;
-  }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        generateParenthesisHelper(result, "", 0, 0, n);
+        return result;
+    }
 
 private:
-  void generateParenthesisHelper(vector<string> &result, string current,
-                                 int openCount, int closedCount, int max) {
-    if (current.length() == 2 * max) {
-      result.push_back(current);
-      return;
+void generateParenthesisHelper(vector<string>& result, string current, int openCount, int closeCount, int max, string indent = "") {
+    cout << indent << "Current: " << current << ", Open: " << openCount << ", Close: " << closeCount << endl;
+
+    if (current.length() == max * 2) {
+        result.push_back(current);
+        cout << indent << "Found: " << current << endl;
+        return;
     }
+
     if (openCount < max) {
-      generateParenthesisHelper(result, current + "(", openCount + 1,
-                                closedCount, max);
+        generateParenthesisHelper(result, current + "(", openCount + 1, closeCount, max, indent + "  ");
     }
-    if (closedCount < openCount) {
-      generateParenthesisHelper(result, current + ")", openCount,
-                                closedCount + 1, max);
+
+    if (closeCount < openCount) {
+        generateParenthesisHelper(result, current + ")", openCount, closeCount + 1, max, indent + "  ");
     }
-  }
+
+    cout << indent << "Backtrack from: " << current << endl;
+}
+
 };
+
 // Driver code for testing
 int main() {
     Solution sol;
