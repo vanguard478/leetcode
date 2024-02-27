9aa9d2270f9da215cb292d775d24062094cf548a
Raza <razamailbox@gmail.com>
2024-02-27 21:40:04 +0100

    added cmake files

--- 14_longest_common_prefix/sol0_invalid.cpp
+++ 14_longest_common_prefix/sol0_invalid.cpp
@@ -1,4 +1,9 @@
 #define MAX_USERS 100000
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
 #define MAX_PASSWORD 10
 int main(void) {
     static char users[MAX_USERS][MAX_PASSWORD + 1];
@@ -6,20 +11,18 @@ int main(void) {
     char password[MAX_PASSWORD + 1];
     int num_users = 0;
     scanf("% d", &num_ops);
-    for (op = 0; op < num_ops; op + +) {
+    for (op = 0; op < num_ops; op++) {
         scanf("% d% s", &op_type, password);
-        if (op_type = = 1) {
+        if (op_type == 1) {
             strcpy(users[num_users], password);
-            num_users + +;
+            num_users++;
 
         } else {
             total = 0;
-            for (j = 0; j < num_users; j + +)
-                if (strstr(users[j], password)) total + +;
-            printf("% d\ n", total);
+            for (j = 0; j < num_users; j++)
+                if (strstr(users[j], password)) total++;
+            printf("% d\n", total);
         }
     }
     return 0;
 }
-
-Daniel Zingaro.Daniel Zingaro - Learn Algorithms(Kindle Locations 837 - 845).Kindle Edition.
--- 26_remove_duplicates_from_sorted_array/solution.cpp
+++ 26_remove_duplicates_from_sorted_array/solution.cpp
@@ -1,51 +1,50 @@
 #include <iostream>
-#include <vector>
 #include <set>
+#include <vector>
 class Solution {
-public:
+   public:
     int removeDuplicates(std::vector<int>& nums) {
-
         int j{1};
         for (int i{1}; i < nums.size(); ++i) {
-            if (nums[i-1] != nums[i]) {
-                nums[j]= nums[i];
+            if (nums[i - 1] != nums[i]) {
+                nums[j] = nums[i];
                 ++j;
             }
         }
         return j;
     }
 };
-class Solution {
-public:
-    int removeDuplicates(std::vector<int>& nums) {
-        int j{0};
-        for (int i{0}; i < nums.size(); ++i) {
-            if (nums[i+1] != nums[i]) {
-                nums[j]= nums[i];
-                ++j;
-            }
-        }
-        return j;
-    }
-};
+// class Solution {
+// public:
+//     int removeDuplicates(std::vector<int>& nums) {
+//         int j{0};
+//         for (int i{0}; i < nums.size(); ++i) {
+//             if (nums[i+1] != nums[i]) {
+//                 nums[j]= nums[i];
+//                 ++j;
+//             }
+//         }
+//         return j;
+//     }
+// };
 int main() {
     std::vector<int> nums = {1, 1, 2, 2, 3, 4, 4, 5};
     Solution solution;
-        std::cout<< "the original nums is \n";
-    for (auto i : nums){
+    std::cout << "the original nums is \n";
+    for (auto i : nums) {
         std::cout << i << " ";
     }
-    std::cout<<std::endl;
+    std::cout << std::endl;
     int length = solution.removeDuplicates(nums);
     std::set<int> uniqueSet(nums.begin(), nums.end());
     std::vector<int> uniqueVec(uniqueSet.begin(), uniqueSet.end());
     for (int num : uniqueVec) {
         std::cout << num << " ";
     }
-    std::cout<<std::endl;
+    std::cout << std::endl;
 
-    std::cout<< "the modified nums is \n";
-    for (auto i : nums){
+    std::cout << "the modified nums is \n";
+    for (auto i : nums) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
